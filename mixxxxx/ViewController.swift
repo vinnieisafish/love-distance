c2ca64bcd475451e4f5f1130351fa877f4f3fb99
stoller <stoller@stollerde-macbook-pro.local>
2020-05-30 17:53:09 +0800

    finish

--- mixxxxx/ViewController.swift
+++ mixxxxx/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var girl: UIImageView!
+    
+    @IBOutlet weak var boy: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func run(_ sender: UISlider) {
+        let speed=sender.value*15
+        girl.frame.origin.x = CGFloat(50+speed*6)
+        boy.frame.origin.x = CGFloat(600-speed)
+    }
+    
+    
 
 }
-
